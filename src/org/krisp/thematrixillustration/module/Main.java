91437d649d2397f58733357b0fa6451c1444b7c7
Zakia Salod <zakia.salod@gmail.com>
2019-06-09 22:15:50 +0200

    initial commit

---
+++ src/org/krisp/thematrixillustration/module/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Bismillahir Rahmaanir Raheem
+ * Almadadh Ya Gause RadiAllahu Ta'alah Anh - Ameen
+ */
+
+package org.krisp.thematrixillustration.module;
+
+public class Main {
+
+    public static void main (String [] args){
+        System.out.println("Alhumdulillah!!!!!!!");
+    }
+}
